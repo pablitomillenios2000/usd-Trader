e2ff624f8c3dbd7ab08777fc1904b43d8989c61f
José-Pablo Escaida Navarro <1deep.coin1@gmail.com>
2024-12-12 22:19:49 +0100

    install dependencies

---
+++ binance-algo/install_dependencies.sh
@@ -0,0 +1,8 @@
+sudo apt update
+sudo apt install python3-pip -y
+pip install websocket-client
+pip install websocket
+pip install python-websocket
+pip install python-binance
+pip install binance
+pip install pandas tqdm
