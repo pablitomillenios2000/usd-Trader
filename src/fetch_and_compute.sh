29369f13b81ff8dd79b0aeb80597cfb700811c6d
José-Pablo Escaida Navarro <1deep.coin1@gmail.com>
2024-12-17 20:53:40 +0100

    clickable localhost

--- src/fetch_and_compute.sh
+++ src/fetch_and_compute.sh
@@ -11,3 +11,5 @@ python3 "recompute.py"
 cd ..
 echo " "
 echo "Data fetched and computed. Please refresh localhost:8000 now"
+echo -e "\e]8;;http://localhost:8000\e\\Open localhost:8000 in the browser\e]8;;\e\\"
+echo " "
