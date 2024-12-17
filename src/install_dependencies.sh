9981005712d4c699090ddd579c50bd39cb260827
José-Pablo Escaida Navarro <1deep.coin1@gmail.com>
2024-12-17 18:41:58 +0100

    websocket conflict

--- src/install_dependencies.sh
+++ src/install_dependencies.sh
@@ -1,8 +1,8 @@
 sudo apt update
 sudo apt install python3-pip -y
 sudo apt install jq -y
-pip install websocket-client 
-pip install websocket
+#pip install websocket-client 
+#pip install websocket
 pip install python-websocket
 pip install python-binance
 pip install binance
