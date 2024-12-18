e96394bc6ffb3b69798679727a67bfc9fed7b672
José-Pablo Escaida Navarro <1deep.coin1@gmail.com>
2024-12-19 00:23:57 +0100

    caddy

--- src/caddy/install_caddy.sh
+++ src/caddy/install_caddy.sh
@@ -1,6 +1,8 @@
-sudo curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo apt-key add -
-sudo curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy.list
+sudo apt install gnupg curl apt-transport-https cdebian-keyring debian-archive-keyring -y
 
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
+
+wget -qO - https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt | sudo tee /etc/apt/sources.list.d/caddy.list
 
 sudo apt update
 sudo apt install caddy -y
